96df1704943a78e7acd233f03ce2d436272444cc
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2020-06-21 23:06:45 +0600

    Updates

---
+++ precache-manifest.2140906ae5f86d0b408cc7da5167e208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457d8df523621e6ecb4a9aa8a9853102",
+    "url": "/pixabay-gallery/index.html"
+  },
+  {
+    "revision": "13a617ffaf3b6602dc0c",
+    "url": "/pixabay-gallery/static/css/main.41ce7317.chunk.css"
+  },
+  {
+    "revision": "bab87538429f1b2b745a",
+    "url": "/pixabay-gallery/static/js/2.bf8f4730.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pixabay-gallery/static/js/2.bf8f4730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a617ffaf3b6602dc0c",
+    "url": "/pixabay-gallery/static/js/main.dfa03370.chunk.js"
+  },
+  {
+    "revision": "af67968f53fa7e8cb46b",
+    "url": "/pixabay-gallery/static/js/runtime-main.f0bb8d65.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.896630272336145bfc925f96eaf3f93f.js"
+  "/pixabay-gallery/precache-manifest.2140906ae5f86d0b408cc7da5167e208.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pixabay-gallery/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
